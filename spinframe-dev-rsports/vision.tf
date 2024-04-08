0d699d101bf7a25727b665bb4ef66956ff29b8c7
Akiva Ashkenazi <akiva.ashkenazi@comm-it.cloud>
2024-04-08 15:37:45 +0300

    Added spinframe-dev-clientapi

--- spinframe-dev-rsports/vision.tf
+++ spinframe-dev-rsports/vision.tf
@@ -1,6 +1,6 @@
 resource "google_project_iam_member" "service_account_visionai_admin" {
-  project = var.project_id
-  role    = "roles/visionai.admin"
-  member  = "serviceAccount:${module.mig_with_https_lb.mig_service_account}"
+  project    = var.project_id
+  role       = "roles/visionai.admin"
+  member     = "serviceAccount:${module.mig_with_https_lb.mig_service_account}"
   depends_on = [module.mig_with_https_lb, google_pubsub_topic.app_topic]
 }
